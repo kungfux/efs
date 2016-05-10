54d1b9b629e45a2f7801a25eba8906688103a453
Alexander Fuks <kungfux2010@gmail.com>
2016-05-10 18:39:15 +0300

    Displaying of program name and help is added

--- project/Program.cs
+++ project/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace ffs_util
 {
@@ -6,17 +8,74 @@ namespace ffs_util
     {
         static void Main(string[] args)
         {
-            var efs = new EatFreeSpace();
+            PrintAbout();
 
-            if (efs.IsPieFileExist())
-                efs.FreeTheSpace();
-            else
+            if (args == null ||
+                args.Length <= 0)
             {
-                Drive.Instance.PrintDriveInfo();
-                Message.Instance.Write("Free space on target drive will be filled up. Press any key to continue...");
-                Console.ReadKey();
-                efs.FillFreeSpace();
+                PrintHelp();
+                return;
             }
+
+            string drive = "";
+
+            for (int a = 0; a < args.Length; a++)
+            {
+                if (args[a] == "--help" ||
+                    args[a] == "/?")
+                {
+                    PrintHelp();
+                    return;
+                }
+
+                if (args[a].StartsWith("-d=") && args[a].Length == 4)
+                {
+                    drive = args[a].Substring(3, 1);
+                    Console.WriteLine(drive);
+                }
+                else
+                {
+                    Console.WriteLine(
+                        string.Format("ssf-util: '{0}' is not a correct command. See 'ffs-util --help'.",
+                        args[a])
+                        );
+                    return;
+                }
+            }
+
+            //var efs = new EatFreeSpace();
+
+            //if (efs.IsPieFileExist())
+            //    efs.FreeTheSpace();
+            //else
+            //{
+            //    Drive.Instance.PrintDriveInfo();
+            //    Message.Instance.Write("Free space on target drive will be filled up. Press any key to continue...");
+            //    Console.ReadKey();
+            //    efs.FillFreeSpace();
+            //}
+        }
+
+        static void PrintAbout()
+        {
+            Console.WriteLine(
+                string.Format("Fill Free Space utility [Version {1}]{0}Copyright (c) 2016 Alexander Fuks{0}", 
+                Environment.NewLine,
+                Assembly.GetExecutingAssembly().GetName().Version.ToString())
+                );
+        }
+
+        static void PrintHelp()
+        {
+            // TODO: Add [-m[=(mode 0|1)]]
+            Console.WriteLine(
+                string.Format(
+                    string.Concat(
+                        "usage: ffs-util [--help] [/?] [-d[=(drive)]]  {0}{0}",
+                        "The keywords used in commands are: {0}",
+                        "   drive   System drive name like C or D {0}"),
+                    Environment.NewLine)
+                );
         }
     }
 }
